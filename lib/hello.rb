c13fc5c55457bc60aa53e0e6749da7a966850d79
Salem Amba <salemamba@gmail.com>
2015-09-12 09:23:06 -0700

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,9 @@
 #  default name  is world
 # Author: Salem Amba(salemamba@gmail.com)
 
-name = ARGV.first || "world"
+# name = ARGV.first || "world"
 # name ||="world!"
+my_name = gets.strip 
 puts "Hello, #{name}!" 
+
 
